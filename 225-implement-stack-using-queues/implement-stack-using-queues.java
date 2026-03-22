1011adbad7bfa93ed1dc73c24a8c492b1d0d0721
anilkumarcs27-bit <anil.kumar.cs27@iilm.edu>
2026-03-22 19:37:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (36.00%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -2,30 +2,35 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class MyStack {
-    Queue<Integer> q;
+    Queue<Integer> q1;
+    Queue<Integer> q2;
 
     public MyStack() {
-        q = new LinkedList<>();
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
 
     public void push(int x) {
-        q.offer(x);
-        int size = q.size();
+        q2.offer(x);
 
-        for (int i = 0; i < size - 1; i++) {
-            q.offer(q.poll());
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
         }
+
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
     }
 
     public int pop() {
-        return q.poll();
+        return q1.poll();
     }
 
     public int top() {
-        return q.peek();
+        return q1.peek();
     }
 
     public boolean empty() {
-        return q.isEmpty();
+        return q1.isEmpty();
     }
 }
