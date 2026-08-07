0e2f4d2c83acf3ef42eaeb163c9ad0f836e0a84b
anilkumarcs27-bit <anil.kumar.cs27@iilm.edu>
2026-08-07 11:06:04 +0530

    Time: 7 ms (31.79%) | Memory: 44.5 MB (67.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] c = new int[26];
+        for(int i=0;i<s.length();i++){
+            c[s.charAt(i)-'a']++;
+            c[t.charAt(i)-'a']--;
+        }
+        for(int a:c){
+            if(a!=0) return false;
+        }
+        return true;
+    }
+}
