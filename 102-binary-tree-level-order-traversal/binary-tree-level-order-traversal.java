1b22631c5fedcd88144a7a2a46ef597064e0e467
anilkumarcs27-bit <anil.kumar.cs27@iilm.edu>
2026-08-14 16:47:34 +0530

    Time: 1 ms (95.86%) | Memory: 47.1 MB (7.77%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,31 +14,47 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new LinkedList<>();
-        if (root == null) return res;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int level_size = queue.size();
-            List<Integer> level = new LinkedList<>();
-
-            for (int i = 0; i < level_size; i++) {
-                TreeNode node = queue.poll();
-                level.add(node.val);
-
-                if (node.left != null)
-                    queue.add(node.left);
-
-                if (node.right != null)
-                    queue.add(node.right);
-            }
-
-            res.add(level);
+    public static void traverse(TreeNode node, int level, List<List<Integer>> res){
+        if(node==null){
+            return;
         }
+        if(level == res.size()){
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(node.val);
+        traverse(node.left,level+1,res);
+        traverse(node.right, level +1, res);
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // List<List<Integer>> res = new LinkedList<>();
+        // if (root == null) return res;
 
+        // Queue<TreeNode> queue = new LinkedList<>();
+        // queue.add(root);
+
+        // while (!queue.isEmpty()) {
+        //     int level_size = queue.size();
+        //     List<Integer> level = new LinkedList<>();
+
+        //     for (int i = 0; i < level_size; i++) {
+        //         TreeNode node = queue.poll();
+        //         level.add(node.val);
+
+        //         if (node.left != null)
+        //             queue.add(node.left);
+
+        //         if (node.right != null)
+        //             queue.add(node.right);
+        //     }
+
+        //     res.add(level);
+        // }
+
+        // return res;
+
+        List<List<Integer>> res = new LinkedList<>();
+        traverse(root,0,res);
         return res;
+
     }
 }
